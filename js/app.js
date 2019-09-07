4ed74416a1355237f0f20b5ccca293804f0a3b8c
Joe Cooper <josephcooper3@gmail.com>
2019-09-07 09:16:20 +0100

    input items now appended to list

--- js/app.js
+++ js/app.js
@@ -2,10 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const handleFormSubmit = function (event) {
     event.preventDefault();
+
     const dogName = event.target.dog_name.value;
     const dogBreed = event.target.dog_breed.value;
     const dateBookedUntil = event.target.date.value;
-    console.log(`${dogName} the ${dogBreed} is booked in kennels until ${dateBookedUntil}!`);
+
+    const dogDetails = `${dogName} the ${dogBreed} is booked in kennels until ${dateBookedUntil}!`;
+
+    const bookedList = document.querySelector('.booked-list');
+    const newListItem = document.createElement('li');
+    bookedList.appendChild(newListItem);
+    newListItem.classList.add('dog-booked');
+    newListItem.textContent = dogDetails;
+
+    event.target.reset()
   };
 
   const form = document.querySelector('form');
